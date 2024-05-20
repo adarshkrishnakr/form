cc5773fe26b0250b2457f4c6e8b3dd3cb79ee938
Adarsh Krishna KR <adarshkrishnakrofficial@gmail.com>
2024-05-20 19:16:36 +0530

    updated

--- lib/screens/availableSlots.dart
+++ lib/screens/availableSlots.dart
@@ -1,9 +1,21 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:project1/provider/formData.dart';
 
 class AvailableSlot extends StatelessWidget {
   const AvailableSlot({super.key});
+
+ 
+  Color getRandomColor() {
+    Random random = Random();
+    return Color.fromARGB(
+      255,
+      random.nextInt(256),
+      random.nextInt(256),
+      random.nextInt(256),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,19 +26,50 @@ class AvailableSlot extends StatelessWidget {
       ),
       body: Consumer<FormData>(
         builder: (context, formData, child) {
-          return ListView.builder(
+          return GridView.builder(
+            padding: const EdgeInsets.all(10.0),
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2, 
+              crossAxisSpacing: 10.0,
+              mainAxisSpacing: 10.0,
+              childAspectRatio: 3 / 2,
+            ),
             itemCount: formData.availableSlots.length,
             itemBuilder: (context, index) {
               final slot = formData.availableSlots[index];
-              return Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: ListTile(
-                  
-                  title: Text(slot["name"]),
-                  subtitle: Text("Price: ${slot["price"]}"),
-                  tileColor: const Color.fromARGB(255, 99, 235, 104), 
+              return Container(
+                decoration: BoxDecoration(
+                  color: getRandomColor(), 
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Center(
+                        child: Text(
+                          slot["name"],
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Center(
+                        child: Text(
+                          "Price: ${slot["price"]}",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
               );
             },
           );
